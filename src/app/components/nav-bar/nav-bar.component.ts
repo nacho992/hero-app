bae31c8833721509e24ba93d65b646979e26ff33
nacho <ignacio.bsso@hotmail.com>
2022-03-02 03:08:31 -0300

    'change-font-and-background'

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -10,6 +10,14 @@ export class NavBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let nav = document.querySelector("nav");
+    window.onscroll = function() {
+      if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+      }else {
+        nav.classList.remove("sticky");
+      }
+    }
   }
 
 }
